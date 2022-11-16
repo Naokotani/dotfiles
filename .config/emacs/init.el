12bfc5ebd47c3096c704d6deba134e9efaf93012
Chris Hughes <naokotani@protonmail.com>
2022-11-16 13:38:05 -0400

    Added popper and some settings for c

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -123,6 +123,21 @@
 (use-package all-the-icons
   :straight t)
 
+(use-package popper
+  :straight t
+  :bind (("C-`"   . popper-toggle-latest)
+         ("M-`"   . popper-cycle)
+         ("C-M-`" . popper-toggle-type))
+  :init
+  (setq popper-reference-buffers
+		'("^\\*eshell.*\\*$" eshell-mode ;eshell as a popup
+		  "^\\*shell.*\\*$"  shell-mode  ;shell as a popup
+		  "^\\*term.*\\*$"   term-mode   ;term as a popup
+		  "^\\*vterm.*\\*$"  vterm-mode  ;vterm as a popup
+          ))
+  (popper-mode +1)
+  (popper-echo-mode +1))
+
 ;;;Completion
 
 (use-package which-key)
@@ -468,6 +483,11 @@
 (setq js-indent-level 2)
 (setq-default tab-width 4)
 (add-hook 'js-mode-hook (lambda () (setq tab-width 2)))
+
+;; C setup
+(add-hook 'c-mode 'eglot-ensure)
+(define-key c-mode-map (kbd "C-c m") 'compile)
+
 
 (use-package emmet-mode
   :hook ((sgml-mode . emmet-mode)
