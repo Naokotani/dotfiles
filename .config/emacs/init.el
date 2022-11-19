ef5b6c43180d4184b09aa3666b9f58c00061560c
Chris Hughes <naokotani@protonmail.com>
2022-11-19 04:03:46 -0400

    Added workflow tags and agenda view

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -510,6 +510,76 @@
         (variable-pitch-mode 1)
         (visual-line-mode 1))
 
+(setq org-todo-keywords
+'((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
+	(sequence "BACKLOG(b)" "PLAN(p)" "READY(r)" "ACTIVE(a)" "REVIEW(v)" "WAIT(w@/!)" "HOLD(h)" "|" "COMPLETED(c)" "CANC(k@)")))
+
+(setq org-tag-alist
+'((:startgroup)
+	; Put mutually exclusive tags here
+	(:endgroup)
+	("@errand" . ?E)
+	("@home" . ?H)
+	("@work" . ?W)
+	("@web" . ?w)
+	("Agenda" . ?a)
+	("Python" . ?p)
+	("Emacs" . ?e)
+	("Publish" . ?P)
+	("Batch" . ?b)
+	("Note" . ?n)
+	("Idea" . ?i)))
+
+;; Configure custom agenda views
+(setq org-agenda-custom-commands
+'(("d" "Dashboard"
+	((agenda "" ((org-deadline-warning-days 7)))
+	(todo "NEXT"
+	((org-agenda-overriding-header "Next Tasks")))
+	(tags-todo "agenda/ACTIVE" ((org-agenda-overriding-header "Active Projects")))))
+
+("n" "Next Tasks"
+	((todo "NEXT"
+	((org-agenda-overriding-header "Next Tasks")))))
+
+("W" "Work Tasks" tags-todo "+@work-@home-@Lock")
+("L" "Lockraid Tasks" tags-todo "+@Lock-@home-@work")
+("H" "Home Tasks" tags-todo "+@home-@Lock-@work")
+
+;; Low-effort next actions
+("e" tags-todo "+TODO=\"NEXT\"+Effort<15&+Effort>0"
+	((org-agenda-overriding-header "Low Effort Tasks")
+	(org-agenda-max-todos 20)
+	(org-agenda-files org-agenda-files)))
+
+("w" "Workflow Status"
+	((todo "ACTIVE"
+		((org-agenda-overriding-header "Active Projects")
+			(org-agenda-files org-agenda-files)))
+	(todo "READY"
+		((org-agenda-overriding-header "Ready for Work")
+			(org-agenda-files org-agenda-files)))
+	(todo "PLAN"
+		((org-agenda-overriding-header "In Planning")
+			(org-agenda-todo-list-sublevels nil)
+			(org-agenda-files org-agenda-files)))
+	(todo "WAIT"
+		((org-agenda-overriding-header "Waiting on External")
+			(org-agenda-files org-agenda-files)))
+	(todo "REVIEW"
+		((org-agenda-overriding-header "In Review")
+			(org-agenda-files org-agenda-files)))
+	(todo "BACKLOG"
+		((org-agenda-overriding-header "Project Backlog")
+			(org-agenda-todo-list-sublevels nil)
+			(org-agenda-files org-agenda-files)))
+	(todo "COMPLETED"
+		((org-agenda-overriding-header "Completed Projects")
+			(org-agenda-files org-agenda-files)))
+	(todo "CANC"
+		((org-agenda-overriding-header "Cancelled Projects")
+			(org-agenda-files org-agenda-files)))))))
+
 ;; Set Faces
 (defun nao/org-font-setup ()
   (require 'org-faces)
