d4840d3c21787da3ef603c6e8d5c4b44eae4f342
Chris Hughes <naokotani@protonmail.com>
2022-11-27 10:08:45 -0400

    added to web publishing and projectile

--- .config/emacs/modules/projects.el
+++ .config/emacs/modules/projects.el
@@ -1,7 +1,11 @@
+;; [[file:../../../../org-roam/20221120034815-emacs.org::*Projects][Projects:1]]
 (use-package projectile
   :straight t
   :init
   (projectile-mode +1)
+  :config
+  (setq projectile-switch-project-action #'projectile-dired)
   :bind (:map projectile-mode-map
               ("s-p" . projectile-command-map)
               ("C-c p" . projectile-command-map)))
+;; Projects:1 ends here
--- .config/emacs/modules/web-publishing.el
+++ .config/emacs/modules/web-publishing.el
@@ -1,9 +1,10 @@
 ;; [[file:../../../../org-roam/20221120034815-emacs.org::*Website Publishing][Website Publishing:1]]
 (setq org-html-validation-link nil            ;; Don't show validation link
-      org-html-head-include-scripts nil       ;; Use our own scripts
+
+	  org-html-head-include-scripts nil       ;; Use our own scripts
 	  org-html-preamble nil
 	  org-html-postamble nil
-      org-html-head-include-default-style nil) ;; Use our own styles
+	  org-html-head-include-default-style nil) ;; Use our own styles
 
 ;; Simple web server
 (use-package simple-httpd
