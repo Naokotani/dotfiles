5cec7359d3240ac381c396d3c7425344df58719d
Chris Hughes <naokotani@protonmail.com>
2023-09-02 08:35:56 -0300

    Updated to emacs 29

---
+++ .config/emacs/modules/keybinds.el
@@ -0,0 +1,4 @@
+;; [[file:../../../../org-roam/20221120034815-emacs.org::*Keybinds][Keybinds:1]]
+(global-set-key (kbd "<f5>") 'async-shell-command)
+(global-set-key (kbd "<f6>") 'eshell)
+;; Keybinds:1 ends here
